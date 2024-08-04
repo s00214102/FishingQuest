4178e8e361d90a5696cb83a34f10fa94713020cc
MarcMcCabe <78020714+MarcMcCabe@users.noreply.github.com>
2024-08-04 08:55:51 +0100

    FishingQuest

---
+++ Assets/_Scripts/Character/Player/PlayerFishing.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CMF;
+
+// on key press
+// - can no longer move 
+public class PlayerFishing : MonoBehaviour
+{
+    private AdvancedWalkerController walkerController;
+
+    private void Awake()
+    {
+
+    }
+}
